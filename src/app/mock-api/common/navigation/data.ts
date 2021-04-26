42e086453849e520f971e0b22df375759ba67cdc
sercan <sercanyemen@gmail.com>
2021-04-26 09:55:41 +0300

    (dependencies) Updated Angular, Angular Material and various other packages (changelog) Added changelog and updated the version number

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -899,7 +899,7 @@ export const defaultNavigation: FuseNavigationItem[] = [
                 icon : 'heroicons_outline:speakerphone',
                 link : '/docs/changelog',
                 badge: {
-                    title  : '12.0.0',
+                    title  : '12.1.0',
                     classes: 'px-2 bg-yellow-300 text-black rounded-full'
                 }
             },
--- src/app/modules/admin/docs/changelog/changelog.ts
+++ src/app/modules/admin/docs/changelog/changelog.ts
@@ -10,20 +10,50 @@ export class ChangelogComponent
 {
     changelog: any[] = [
 
+        // v12.1.0
+        {
+            version    : 'v12.1.0',
+            releaseDate: 'April 26, 2021',
+            changes    : [
+                {
+                    type: 'Added',
+                    list: [
+                        '(apps/academy) New and improvement version of Academy app',
+                        '(icons) Material Solid icons'
+                    ]
+                },
+                {
+                    type: 'Changed',
+                    list: [
+                        '(dependencies) Updated Angular, Angular Material and various other packages',
+                        '(icons) Updated Heroicons',
+                        '(icons) Updated Material Icons',
+                        '(apps/file-manager) Better grid for File Manager app',
+                        '(layouts/classy) Removed footer for better \'classy\' look'
+                    ]
+                },
+                {
+                    type: 'Fixed',
+                    list: [
+                        '(apps/contacts) Clicking on the checkbox on Tag select panel acts weird'
+                    ]
+                }
+            ]
+        },
         // v12.0.0
         {
             version    : 'v12.0.0',
             releaseDate: 'April 16, 2021',
             changes    : [
                 {
-                    type: 'Breaking Changes',
+                    type: 'Added',
                     list: [
                         'This is the new major version of the Fuse and it\'s completely different from previous versions with no upgrade path',
                         'This version requires a clean installation'
                     ]
                 },
                 {
-                    type: 'Features',
+                    type: 'Changed',
                     list: [
                         'Improved the look and feel',
                         'Re-wrote the entire template from scratch using Tailwind',
