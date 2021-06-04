fc1e7b02b0040d5e3b5034e65e8bbc8c1f595d72
sercan <sercanyemen@gmail.com>
2021-06-04 22:28:53 +0300

    (data/navigation) Fixed the "Invoice" icon, added missing dashboard links to the "futuristicNavigation" data

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -596,7 +596,7 @@ export const defaultNavigation: FuseNavigationItem[] = [
                 id      : 'pages.invoice',
                 title   : 'Invoice',
                 type    : 'collapsable',
-                icon    : 'heroicons_outline:exclamation-circle',
+                icon    : 'heroicons_outline:calculator',
                 children: [
                     {
                         id      : 'pages.invoice.printable',
@@ -1157,140 +1157,16 @@ export const compactNavigation: FuseNavigationItem[] = [
 ];
 export const futuristicNavigation: FuseNavigationItem[] = [
     {
+        id      : 'dashboards',
+        title   : 'DASHBOARDS',
+        type    : 'group',
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+    {
         id      : 'apps',
         title   : 'APPS',
         type    : 'group',
-        children: [
-            {
-                id   : 'apps.dashboards.analytics',
-                title: 'Analytics',
-                type : 'basic',
-                icon : 'heroicons_outline:chart-pie',
-                link : '/dashboards/analytics'
-            },
-            {
-                id   : 'apps.dashboards.project',
-                title: 'Project',
-                type : 'basic',
-                icon : 'heroicons_outline:clipboard-check',
-                link : '/dashboards/project'
-            },
-            {
-                id   : 'apps.academy',
-                title: 'Academy',
-                type : 'basic',
-                icon : 'heroicons_outline:academic-cap',
-                link : '/apps/academy'
-            },
-            {
-                id   : 'apps.calendar',
-                title: 'Calendar',
-                type : 'basic',
-                icon : 'heroicons_outline:calendar',
-                link : '/apps/calendar'
-            },
-            {
-                id   : 'apps.chat',
-                title: 'Chat',
-                type : 'basic',
-                icon : 'heroicons_outline:chat-alt',
-                link : '/apps/chat'
-            },
-            {
-                id   : 'apps.contacts',
-                title: 'Contacts',
-                type : 'basic',
-                icon : 'heroicons_outline:user-group',
-                link : '/apps/contacts'
-            },
-            {
-                id      : 'apps.ecommerce',
-                title   : 'ECommerce',
-                type    : 'collapsable',
-                icon    : 'heroicons_outline:shopping-cart',
-                children: [
-                    {
-                        id   : 'apps.ecommerce.inventory',
-                        title: 'Inventory',
-                        type : 'basic',
-                        link : '/apps/ecommerce/inventory'
-                    }
-                ]
-            },
-            {
-                id   : 'apps.file-manager',
-                title: 'File manager',
-                type : 'basic',
-                icon : 'heroicons_outline:shopping-cart',
-                link : '/apps/file-manager'
-            },
-            {
-                id      : 'apps.help-center',
-                title   : 'Help center',
-                type    : 'collapsable',
-                icon    : 'heroicons_outline:support',
-                link    : '/apps/help-center',
-                children: [
-                    {
-                        id        : 'apps.help-center.home',
-                        title     : 'Home',
-                        type      : 'basic',
-                        link      : '/apps/help-center',
-                        exactMatch: true
-                    },
-                    {
-                        id   : 'apps.help-center.faqs',
-                        title: 'FAQs',
-                        type : 'basic',
-                        link : '/apps/help-center/faqs'
-                    },
-                    {
-                        id   : 'apps.help-center.guides',
-                        title: 'Guides',
-                        type : 'basic',
-                        link : '/apps/help-center/guides'
-                    },
-                    {
-                        id   : 'apps.help-center.support',
-                        title: 'Support',
-                        type : 'basic',
-                        link : '/apps/help-center/support'
-                    }
-                ]
-            },
-            {
-                id   : 'apps.mailbox',
-                title: 'Mailbox',
-                type : 'basic',
-                icon : 'heroicons_outline:mail',
-                link : '/apps/mailbox',
-                badge: {
-                    title  : '27',
-                    classes: 'px-2 bg-black bg-opacity-25 text-white rounded-full'
-                }
-            },
-            {
-                id   : 'apps.notes',
-                title: 'Notes',
-                type : 'basic',
-                icon : 'heroicons_outline:pencil-alt',
-                link : '/apps/notes'
-            },
-            {
-                id   : 'apps.scrumboard',
-                title: 'Scrumboard',
-                type : 'basic',
-                icon : 'heroicons_outline:view-boards',
-                link : '/apps/scrumboard'
-            },
-            {
-                id   : 'apps.tasks',
-                title: 'Tasks',
-                type : 'basic',
-                icon : 'heroicons_outline:check-circle',
-                link : '/apps/tasks'
-            }
-        ]
+        children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
     },
     {
         id   : 'others',
