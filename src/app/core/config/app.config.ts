efdfa6418aa166cc18db99f6e60a20998311b2b1
sercan <sercanyemen@gmail.com>
2021-06-10 14:56:21 +0300

    (core) Go back to the 'classy' layout by default

--- src/app/core/config/app.config.ts
+++ src/app/core/config/app.config.ts
@@ -24,7 +24,7 @@ export interface AppConfig
  * FuseConfigService and its methods.
  */
 export const appConfig: AppConfig = {
-    layout: 'classic',
+    layout: 'classy',
     scheme: 'light',
     theme : 'default'
 };
