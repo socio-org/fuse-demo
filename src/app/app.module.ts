dfc67ef34aa0e7bf7427c1f51001128a9b476c87
Habel <habel.marottipuzha@gmail.com>
2021-06-14 00:15:27 +0530

    fix base url for transloco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,42 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ExtraOptions, PreloadAllModules, RouterModule } from '@angular/router';
-import { MarkdownModule } from 'ngx-markdown';
-import { FuseModule } from '@fuse';
-import { FuseConfigModule } from '@fuse/services/config';
-import { FuseMockApiModule } from '@fuse/lib/mock-api';
-import { CoreModule } from 'app/core/core.module';
-import { appConfig } from 'app/core/config/app.config';
-import { mockApiServices } from 'app/mock-api';
-import { LayoutModule } from 'app/layout/layout.module';
-import { AppComponent } from 'app/app.component';
-import { appRoutes } from 'app/app.routing';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ExtraOptions, PreloadAllModules, RouterModule} from '@angular/router';
+import {MarkdownModule} from 'ngx-markdown';
+import {FuseModule} from '@fuse';
+import {FuseConfigModule} from '@fuse/services/config';
+import {FuseMockApiModule} from '@fuse/lib/mock-api';
+import {CoreModule} from 'app/core/core.module';
+import {appConfig} from 'app/core/config/app.config';
+import {mockApiServices} from 'app/mock-api';
+import {LayoutModule} from 'app/layout/layout.module';
+import {AppComponent} from 'app/app.component';
+import {appRoutes} from 'app/app.routing';
+import {APP_BASE_HREF, PlatformLocation} from "@angular/common";
 
 const routerConfig: ExtraOptions = {
     scrollPositionRestoration: 'enabled',
-    preloadingStrategy       : PreloadAllModules
+    preloadingStrategy: PreloadAllModules
 };
+
+/**
+ * This function is used internal to get a string instance of the `<base href="" />` value from `index.html`.
+ * This is an exported function, instead of a private function or inline lambda, to prevent this error:
+ *
+ * `Error encountered resolving symbol values statically.`
+ * `Function calls are not supported.`
+ * `Consider replacing the function or lambda with a reference to an exported function.`
+ *
+ * @param platformLocation an Angular service used to interact with a browser's URL
+ * @return a string instance of the `<base href="" />` value from `index.html`
+ */
+export const getBaseHref = (platformLocation: PlatformLocation): string => platformLocation.getBaseHrefFromDOM() || '/';
 
 @NgModule({
     declarations: [
         AppComponent
     ],
-    imports     : [
+    imports: [
         BrowserModule,
         BrowserAnimationsModule,
         RouterModule.forRoot(appRoutes, routerConfig),
@@ -41,10 +55,16 @@ const routerConfig: ExtraOptions = {
         // 3rd party modules that require global configuration via forRoot
         MarkdownModule.forRoot({})
     ],
-    bootstrap   : [
+    providers: [
+        {
+            provide: APP_BASE_HREF,
+            useFactory: getBaseHref,
+            deps: [PlatformLocation]
+        }
+    ],
+    bootstrap: [
         AppComponent
     ]
 })
-export class AppModule
-{
+export class AppModule {
 }
