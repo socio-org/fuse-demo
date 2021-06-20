0c0ef40de38fd48598f4417c85a2603c768d70b5
sercan <sercanyemen@gmail.com>
2021-06-20 21:29:37 +0300

    (docs) Added docs about navigation tooltip

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -1135,6 +1135,7 @@ export const compactNavigation: FuseNavigationItem[] = [
     {
         id      : 'dashboards',
         title   : 'Dashboards',
+        tooltip : 'Dashboards',
         type    : 'aside',
         icon    : 'heroicons_outline:home',
         children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
@@ -1142,6 +1143,7 @@ export const compactNavigation: FuseNavigationItem[] = [
     {
         id      : 'apps',
         title   : 'Apps',
+        tooltip : 'Apps',
         type    : 'aside',
         icon    : 'heroicons_outline:qrcode',
         children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
@@ -1149,6 +1151,7 @@ export const compactNavigation: FuseNavigationItem[] = [
     {
         id      : 'pages',
         title   : 'Pages',
+        tooltip : 'Pages',
         type    : 'aside',
         icon    : 'heroicons_outline:document-duplicate',
         children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
@@ -1156,6 +1159,7 @@ export const compactNavigation: FuseNavigationItem[] = [
     {
         id      : 'user-interface',
         title   : 'UI',
+        tooltip : 'UI',
         type    : 'aside',
         icon    : 'heroicons_outline:collection',
         children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
@@ -1163,6 +1167,7 @@ export const compactNavigation: FuseNavigationItem[] = [
     {
         id      : 'navigation-features',
         title   : 'Navigation',
+        tooltip : 'Navigation',
         type    : 'aside',
         icon    : 'heroicons_outline:menu',
         children: [] // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
