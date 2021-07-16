8b977c0eeba38bc7ab8a37aba136e3ba5f5c9cce
sercan <sercanyemen@gmail.com>
2021-07-16 12:00:46 +0300

    Updated Angular, Angular Material and couple other dependencies & increased the version number

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -967,7 +967,7 @@ export const defaultNavigation: FuseNavigationItem[] = [
                 icon : 'heroicons_outline:speakerphone',
                 link : '/docs/changelog',
                 badge: {
-                    title  : '13.2.0',
+                    title  : '13.3.0',
                     classes: 'px-2 bg-yellow-300 text-black rounded-full'
                 }
             },
