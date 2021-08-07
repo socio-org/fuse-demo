d97a7b74ac3046b797abe56e5e8bc51af7969468
Habel <habel.marottipuzha@gmail.com>
2021-08-07 15:48:06 +0530

    change to hash routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ExtraOptions, PreloadAllModules, RouterModule} from '@angular/router';
-import {MarkdownModule} from 'ngx-markdown';
-import {FuseModule} from '@fuse';
-import {FuseConfigModule} from '@fuse/services/config';
-import {FuseMockApiModule} from '@fuse/lib/mock-api';
-import {CoreModule} from 'app/core/core.module';
-import {appConfig} from 'app/core/config/app.config';
-import {mockApiServices} from 'app/mock-api';
-import {LayoutModule} from 'app/layout/layout.module';
-import {AppComponent} from 'app/app.component';
-import {appRoutes} from 'app/app.routing';
-import {APP_BASE_HREF, PlatformLocation} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ExtraOptions, PreloadAllModules, RouterModule } from '@angular/router';
+import { MarkdownModule } from 'ngx-markdown';
+import { FuseModule } from '@fuse';
+import { FuseConfigModule } from '@fuse/services/config';
+import { FuseMockApiModule } from '@fuse/lib/mock-api';
+import { CoreModule } from 'app/core/core.module';
+import { appConfig } from 'app/core/config/app.config';
+import { mockApiServices } from 'app/mock-api';
+import { LayoutModule } from 'app/layout/layout.module';
+import { AppComponent } from 'app/app.component';
+import { appRoutes } from 'app/app.routing';
+import { APP_BASE_HREF, PlatformLocation } from '@angular/common';
 
 const routerConfig: ExtraOptions = {
     scrollPositionRestoration: 'enabled',
+    useHash: true,
     preloadingStrategy: PreloadAllModules
 };
 
