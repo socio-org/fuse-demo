fe7e2514a657c1954d2ed837134459c6b510a350
sercan <sercanyemen@gmail.com>
2021-08-31 09:43:10 +0300

    Updated dependencies, increased the version number & updated the changelog

--- src/app/modules/admin/docs/changelog/changelog.ts
+++ src/app/modules/admin/docs/changelog/changelog.ts
@@ -10,6 +10,33 @@ export class ChangelogComponent
 {
     changelog: any[] = [
 
+        // v13.6.0
+        {
+            version    : 'v13.6.0',
+            releaseDate: 'Aug 31, 2021',
+            changes    : [
+                {
+                    type: 'Added',
+                    list: [
+                        '(QuickChat) Added the QuickChat bar'
+                    ]
+                },
+                {
+                    type: 'Changed',
+                    list: [
+                        '(dependencies) Updated Angular & Angular Material to v12.2.3',
+                        '(dependencies) Updated various other packages',
+                        '(layout) Separated the Settings drawer from the layout component'
+                    ]
+                },
+                {
+                    type: 'Fixed',
+                    list: [
+                        '(@fuse/drawer) Final opacity of the overlay is not permanent due to player being destroyed right after the animation'
+                    ]
+                }
+            ]
+        },
         // v13.5.0
         {
             version    : 'v13.5.0',
